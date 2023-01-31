b482b8a91b71deea453cc9527a682d2fb9dda132
cdugg <duggancormac21@gmail.com>
2023-01-30 16:52:25 -0800

    initial ibkr connection market orders and connection and disconnect added

--- APILayer/IBKRAPI.py
+++ APILayer/IBKRAPI.py
@@ -1,21 +1,72 @@
+#Imports
+import ibapi
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.order import *
+import numpy as np
+import pandas as pd
+import pytz
+import math
+from datetime import datetime, timedelta
+import threading
+import time
+#Vars
+ORDERID = 4
+
+#Class for Interactive Brokers Connection
+class IBApi(EWrapper,EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    # Get next order id we can use
+    def incrementOrderID(self):
+        global ORDERID
+        ORDERID += 1
+
+
 class InteractiveAPI:
-    def __init__():
-        pass
+    def __init__(self):
+        self.orders = {}
 
-    def long_order():
-        pass
+    def connect(self):
+        self.ib = IBApi()
+        self.ib.connect("127.0.0.1", 7497, 1)
+        ib_thread = threading.Thread(target=self.run_loop, daemon=True)
+        ib_thread.start()
+        time.sleep(1)
 
-    def short_order():
-        pass
+    def disconnect(self):
+        self.ib.disconnect()
 
-    def set_stop():
-        pass
+    def run_loop(self):
+        self.ib.run()
 
-    def close_positions():
-        pass
+    def create_contract(self, symbol, secType = "STK", exchange = "SMART", currency = "USD"):
+        contract = Contract()
+        contract.symbol = symbol
+        contract.secType = secType
+        contract.exchange = exchange
+        contract.currency = currency
+        return contract
 
-    def get_positions():
-        pass
+    def create_market_order(self, action, quantity):
+        order = Order()
+        order.orderId = ORDERID
+        order.orderType = "MKT"
+        order.action = action
+        order.totalQuantity = quantity
+        order.eTradeOnly = ''
+        order.firmQuoteOnly = ''
+        self.orders[ORDERID] = set([order])
+        self.ib.incrementOrderID()
+        return ORDERID - 1, order
 
-    def get_account_value():
-        pass
+    def place_order(self, orderId, contract, order):
+        self.ib.placeOrder(orderId, contract, order)
+        self.orders[orderId].add(contract.symbol)
+
+    def print_orders(self, placed=False):
+        for id in self.orders.keys():
+            if (len(self.orders[id]) > 1 and placed) or not placed:
+                print(id, ": ", self.orders[id])
--- PortfolioManager/manager.py
+++ PortfolioManager/manager.py
@@ -2,3 +2,21 @@
 # Never allow any account to trade if it will put the account value at risk of going below $25,000
 # Based on trust level of Model used commit different amounts to trade
 # Use corresponding APIs to the account being used
+
+# To test apis being set up lets try to send one order to each client
+# Buy 1 share of something cheap
+import sys
+sys.path.append('../')
+from APILayer import IBKRAPI
+
+IBKR = IBKRAPI.InteractiveAPI()
+IBKR.connect()
+contract = IBKR.create_contract("AAPL")
+id, order = IBKR.create_market_order("BUY", 1)
+id, order = IBKR.create_market_order("BUY", 2)
+IBKR.place_order(id, contract, order)
+print("All")
+IBKR.print_orders()
+print("Placed")
+IBKR.print_orders(True)
+IBKR.disconnect()
